7380ec77b43e1dd180cd25e722b64ec0db7c861f
aznyqjai <aznyqjai@gmail.com>
2016-11-14 00:22:23 -0500

    all functionality should work

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	console.log("$ fired");
-	check_user_logged()
+	check_user_logged();
+	carted_total();
+	populate_cart();
 		//checking to see if it's cart page. if it is, fired blabalbal
 		// $(function(){
 		//   if($('body').is('.cart')){
@@ -13,20 +15,81 @@ $(document).ready(function(){
 })
 
 
+
 function add_product(){
-	Cookies.set("item_added",true);
-	Cookies.set($("#product_ID").html(), JSON.stringify({price:$("#product_price").html(), quantity: 1}));
-	console.log("add_product fired");
-	//console.log(Cookies.get)
+	var item_carted=Cookies.get("item_carted");
+	// add object to array if it's cookied
+	if (item_carted){
+		console.log("item_carted cookied");
+		var item = $.parseJSON(Cookies.get("item_carted"));
+		item.push({product_id:$("#product_ID").html(), price:$("#product_price").html(),  picture_source: $("#product_pic")[0].src, quantity: 1});
+
+		Cookies.set("item_carted", JSON.stringify(item));
+		location.reload();
+
+	}
+	// create cookie if not found
+	else{
+		console.log("no cookied");
+		Cookies.set("item_carted", JSON.stringify([{product_id:$("#product_ID").html(), price:$("#product_price").html(),  picture_source: $("#product_pic")[0].src, quantity: 1}]));
+		console.log("add_product fired");
+		//console.log(Cookies.get)
+		cookie_item_obj=$.parseJSON(Cookies.get("item_carted"));
+		console.log("cookie created");
+		location.reload();
+	}
+
+	//$("#item_carted").html(item_carted.length);
 }
 
+function carted_total(){
+	var item_carted=Cookies.get("item_carted");
+	if (item_carted){
+		var item=$.parseJSON(Cookies.get("item_carted"));
+		console.log("item_length is:  " + item.length);
+		$("#item_carted").html(item.length);
+	}
+	else{
+		console.log("item length is 0");
+	}
+}
 
 function populate_cart(){
+	var item_carted=Cookies.get("item_carted");
+	if (item_carted){
+		var item=$.parseJSON(Cookies.get("item_carted"));
+		var total_cost=0;
+		for (var i=0; i<item.length; i++){
+		$("#itemize").append(' <div id="pro_1" class="row"> <div class="col-xs-2"><img class="img-responsive" src="' + item[i].picture_source + '">'+  '</div> <div class="col-xs-4"> <h4 class="product-name"><strong>'+item[i].product_id+'</strong></h4><h4><small>Product description</small></h4> </div> <div class="col-xs-6"> <div class="col-xs-6 text-right"> <h6><strong>' + item[i].price + ' <span class="text-muted">x</span></strong></h6> </div> <div class="col-xs-4"> <input id ="inputid1" type="text" class="form-control input-sm" value="1"> </div> <div class="col-xs-2"> <button type="button" class="btn btn-link btn-xs"> <span class="glyphicon glyphicon-trash"> </span></button></div></div><hr>');
+		parseInt(total_cost+=parseInt(item[i].price));
+		}
+		$("#total_cost").html('$'+total_cost);
+	}
 
 }
 
 function checkOut(){
 	console.log("checkout fired");
+	//check_user_logged();
+	if (Cookies.get("userlogged")){
+		if (Cookies.get("item_carted")){
+		$("#container_page").hide();
+		$("#success_checkout").show();
+		console.log("checking out")
+		Cookies.expire("item_carted");
+		//location.reload();			
+		}
+		else {
+			alert("nothing in cart");
+		}
+	}
+
+	else {
+		//alert("Login First");
+		$("#container_page").effect("shake");
+		location.replace("signin.html");
+	}
+
 }
 
 
@@ -49,7 +112,8 @@ function sign_in(){
 		//window.response=response;
 		if (response.result===null){
 			console.log("wrong login !!");
-		//document.getElementById('welcome').innerHTML="Wrong Credential";
+			$("#myModal").effect( "shake" );
+		
 		}
 		else {
 			console.log("firstName is: "+ response.result.fname);
@@ -73,12 +137,18 @@ function log_out(){
 
 function check_user_logged(){
 	if (Cookies.get("userlogged")) {
+		console.log("user logged in");
 		var name=$.parseJSON(Cookies.get("userlogged")).name;
 		$("#youraccount").html(name);
 		$("#myModal").hide();
 		$("#welcome_modal").show();
 		$("#welcome_text").html("WELCOME BACK "+ name);		
-	};
+	}
+	else {
+		console.log("user not logged in");
+		//location.replace("signin.html");
+	}
+
 }
 
 
@@ -89,7 +159,7 @@ function signup(){
 	var email = $("#signup_userid").val(); 				//"qzhang@gmail.com";
 	var password = $("#signup_password").val();
 	var reenterpassword = $("#reenterpassword").val();
-	if (password==reenterpassword){
+	if (password==reenterpassword && password!==""){
 		console.log("same password and it is: " + password);
 
 		$.ajax({
@@ -113,6 +183,9 @@ function signup(){
 			}
 		  }
 		})
+	}
+	else {
+		$("#myModal").effect( "shake" );
 	}
 }
 
